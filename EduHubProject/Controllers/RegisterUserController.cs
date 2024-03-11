33ba1c41e3e36e239a65f8b59eb160abc12965ff
abhishekb13402 <abhishekbhatt13402@gmail.com>
2024-03-11 17:04:47 +0530

    apply authorization

--- EduHubProject/Controllers/RegisterUserController.cs
+++ EduHubProject/Controllers/RegisterUserController.cs
@@ -1,8 +1,13 @@
 ﻿using EduHubProject.Data;
 using EduHubProject.Models;
 using EduHubProject.Repository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace EduHubProject.Controllers
 {
@@ -12,23 +17,61 @@ namespace EduHubProject.Controllers
     {
         private readonly EduHubDBContext eduHubDB;
         RegUserRepository reguserrepository;
-
-        public RegisterUserController(EduHubDBContext eduHubDB)
+        IConfiguration configuration;
+        public RegisterUserController(EduHubDBContext eduHubDB,IConfiguration configuration)
         {
             this.eduHubDB = eduHubDB;
             reguserrepository = new RegUserRepository(eduHubDB);
+            this.configuration = configuration;
         }
 
         [HttpGet]
+        [Authorize]
         public object GetAllUsers()
         {
             return reguserrepository.GetAll();
         }
 
         [HttpGet("{id}")]
+        [Authorize]
         public object? GetUser(int id)
         {
             return reguserrepository.GetRegisterUserById(id);
+        }
+
+        [HttpPost("Authentication")]
+        public object AuthenticateUser(string username, string password)
+        {
+            bool isValidUser = reguserrepository.AuthenticateUser(username, password);
+            if (isValidUser)
+            {
+                return generateToken(username);
+            }
+            else
+            {
+                return new Exception("Invalid user name and password");
+            }
+        }
+
+        private string generateToken(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,username),
+                //new Claim(ClaimTypes.Email,user.Email),
+
+            };
+            var token = new JwtSecurityToken(this.configuration["Jwt:Issuer"],
+                this.configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
         [HttpPost]
@@ -43,7 +86,7 @@ namespace EduHubProject.Controllers
             return reguserrepository.UpdateRegisterUser(registerUser);
         }
 
-        [HttpDelete] 
+        [HttpDelete]
         public object DeleteUser(int id)
         {
             return reguserrepository.DeleteRegisterUser(id);
--- EduHubProject/Program.cs
+++ EduHubProject/Program.cs
@@ -1,18 +1,65 @@
 using EduHubProject.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+            };
+        });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
 
 var provider = builder.Services.BuildServiceProvider();
 var config = provider.GetRequiredService<IConfiguration>();
 builder.Services.AddDbContext<EduHubDBContext>(options => options.UseSqlServer(config.GetConnectionString("dbcs")));
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EduHub.API", Version = "v1" });
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type=ReferenceType.SecurityScheme,
+                Id="Bearer"
+            },
+            Scheme="oauth2",
+            Name="Bearer",
+            In=ParameterLocation.Header
+        },
+        new List<string>()
+    }
+    });
+});
 
 var app = builder.Build();
 
@@ -24,7 +71,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
